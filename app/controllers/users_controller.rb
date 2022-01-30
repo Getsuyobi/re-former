bd548d045aa4a7cba4caa47975ad757f3aa604ed
Getsuyobi <youcef93200@gmail.com>
2022-01-30 20:26:26 +0100

    Added "new" and "edit" forms using form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
@@ -9,6 +11,20 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update 
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end 
   end
 
   private
